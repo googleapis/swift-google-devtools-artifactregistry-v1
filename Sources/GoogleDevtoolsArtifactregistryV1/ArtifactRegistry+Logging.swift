554566f31d6d5360245fa9d19dd10d81930e7eaf
Carlos O'Ryan <coryan@google.com>
2026-07-27 13:05:32 -0400

    chore: update librarian circa 2026-07-24 (#20)
    Update the librarian version. In this version the `gcp.artifact.id`
    logging attribute is set to the package name, as it should be.

--- Sources/GoogleDevtoolsArtifactregistryV1/ArtifactRegistry+Logging.swift
+++ Sources/GoogleDevtoolsArtifactregistryV1/ArtifactRegistry+Logging.swift
@@ -33,7 +33,7 @@ extension Clients {
 
     public init(_ inner: any ArtifactRegistryStub, logger: Logger) {
       var logger = logger
-      logger[metadataKey: "gcp.artifact.id"] = "GoogleDevtoolsArtifactregistryV1"
+      logger[metadataKey: "gcp.artifact.id"] = "google-devtools-artifactregistry-v1"
       logger[metadataKey: "gcp.client.service"] = "artifactregistry"
       logger[metadataKey: "gcp.experimental.swift.client"] = "ArtifactRegistry"
       self.inner = inner
